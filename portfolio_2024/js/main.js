bd666df6cb72b0b2c8908932137dc6061508090d
DeepDiv <cosy-@naver.com>
2024-08-20 18:29:17 +0900

    0820_모바일개선

--- portfolio_2024/js/main.js
+++ portfolio_2024/js/main.js
@@ -1,4 +1,7 @@
 
+let winSc = 0;
+const winMo = window.matchMedia("(max-width: 1024px)").matches;
+
 // 퀵메뉴 영역
   function nmv(seq){
       const offset = $("#content" + seq).offset();
@@ -20,7 +23,7 @@
           description: "2024년 포트폴리오 시안을 작업하고 퍼블리싱하였습니다. 인터랙티브 효과는 GSAP 모션 라이브러리를 주로 사용하여 구현하였으며, 메인 영역에서는 SVG를 활용해 텍스트 애니메이션에 중점을 두었습니다. 배경은 캔버스를 이용해 물결 애니메이션을 추가하였으며, 슬라이드 영역에는 Swiper 라이브러리 사용하였습니다. 데이터는 배열스크립트로 구현하여 동적으로 호출하였습니다", // 설명
           contribution: "100%", // 기여도 퍼센트
           workScope: "전체", // 작업영역
-          link: "https://steam.kofac.re.kr/main", // 링크
+          link: "https://deepdivdiv.github.io/pub/portfolio_2024/index.html", // 링크
           view: "https://mud-chess-378.notion.site/2024-a606c7d2830349fa8d3a8d1ccc4890e7", // 뷰페이지(노션)
       },
       {
@@ -39,7 +42,18 @@
           imgAlt: "찾아가는 학교 연수 메인 이미지",
           skills: ["HTML5", "JAVASCRIPT", "JQUERY", "CSS3", "GIT"], // 스킬
           title: "찾아가는 학교 연수", // 제목
-          description: "디지털 혁신을 통해 학교를 변화시킬 찾아오는 학교 연수 사이트를 퍼블리싱 하였습니다. 메인 페이지에서는 시작/정지 버튼을 통해 Progress Bar를 원형 게이지(Circle Gauge) 형태로 구현하였습니다. 메인 페이지 외에도 모든 서브 페이지에 대한 퍼블리싱 작업을 완료하였으며, 작업 후 개발자와의 협업이 필요한 부분에서는 적극적으로 참여하였습니다..", // 설명
+          description: "디지털 혁신을 통해 학교를 변화시킬 찾아오는 학교 연수 사이트를 퍼블리싱 하였습니다. 메인 페이지에서는 시작/정지 버튼을 통해 Progress Bar를 원형 게이지(Circle Gauge) 형태로 구현하였습니다. 메인 페이지 외에도 모든 서브 페이지에 대한 퍼블리싱 작업을 완료하였으며, 작업 후 개발자와의 협업이 필요한 부분에서는 적극적으로 참여하였습니다.", // 설명
+          contribution: "100%", // 기여도 퍼센트
+          workScope: "전체", // 작업영역
+          link: "https://dx.kofac.re.kr/dx/main", // 링크
+          view: "https://mud-chess-378.notion.site/b5ddd54a6e754259b8a63d02533ff861", // 뷰페이지(노션)
+      },
+      {
+          imgSrc: fileRoot + "2023_jt.JPG", // 이미지 경로
+          imgAlt: "전통문화포털 메인 이미지",
+          skills: ["JSP", "JAVASCRIPT", "JQUERY", "CSS3", "SVN"], // 스킬
+          title: "전통문화포털", // 제목
+          description: "2023년 리뉴얼 퍼블리싱 작업을 완료했습니다. 웹표준 및 웹접근성을 준수하였습니다. 기존 사이트의 데이터를 유지하면서 메인 페이지만 새롭게 디자인 및 퍼블리싱하는 작업이었으며, 기존 데이터와 신규 작업 영역을 문제없이 마이그레이션했습니다. 이 과정에서 추가 개발 없이 퍼블리싱과 마이그레이션 작업을 독립적으로 수행했으며, 이를 통해 JSP를 보다 심도 있게 다룰 수 있는 기회를 가졌습니다.", // 설명
           contribution: "100%", // 기여도 퍼센트
           workScope: "전체", // 작업영역
           link: "https://dx.kofac.re.kr/dx/main", // 링크
@@ -80,7 +94,6 @@
       },
     ];
     const designSlidesData = [
-
       {
           imgSrc: fileRoot + "2024_oye.png", // 이미지 경로
           imgAlt: "2024 오영은 포트폴리오 메인 이미지",
@@ -427,12 +440,7 @@
     
   });
   let introText = gsap.timeline();
-  introText.to('.m_inner img', {opacity: 1, duration:0.5, x: 0, delay: 1.4})
-  .to('.m_copy span', {opacity: 1, duration:0.5, y: 0, delay: .1})
-  .to('.svg-txt path', {opacity: 1, duration:0.5, strokeDashoffset:0, strokeDasharray:85})
-  .to('.svg-txt path', {fill:"#fff"})
-  .to('.m_copy p', {opacity: 1, duration:0.5, y:0,})
-  .to('.scroll', {opacity: 1, duration:0.5});
+
 
   // 캔버스
   gsap.to(".waveBG", {
@@ -452,25 +460,42 @@
     },
     y: "30vh",
   });
-  // 스크롤 오브제
-  gsap.to(".scroll span", {
 
-    scrollTrigger: {
-      trigger: "#content01",
-      start: "top center",
-      scrub: 1,
-    },
-    rotate: 180, duration: 10,
-  });
-  gsap.to(".scroll", {
-    scrollTrigger: {
-      trigger: "#content01",
-      start: "center bottom",
-      scrub: 5,
-    },
-    y: "150%", duration: 0.1,
-  });
-  
+    // 스크롤 오브제
+    gsap.to(".scroll span", {
+
+      scrollTrigger: {
+        trigger: "#content01",
+        start: "top center",
+        scrub: 1,
+      },
+      rotate: 180, duration: 10,
+    });
+    gsap.to(".scroll", {
+      scrollTrigger: {
+        trigger: "#content01",
+        start: "center bottom",
+        scrub: 5,
+      },
+      y: "150%", duration: 0.1,
+    });
+    
+    if(winMo) {
+      gsap.killTweensOf(".scroll");
+      introText.to('.m_inner img', {opacity: 1, duration:0.5, x: 0, delay: 1.4})
+      .to('.m_copy span', {opacity: 1, duration:0.5, y: 0, delay: .1})
+      .to('.m_copy h2', {opacity: 1, duration:0.5, y: 0, delay: .1})
+      .to('.m_copy p', {opacity: 1, duration:0.5, y:0,})
+      .to('.scroll', {opacity: 1, duration:0.5});
+    } else {
+      introText.to('.m_inner img', {opacity: 1, duration:0.5, x: 0, delay: 1.4})
+      .to('.m_copy span', {opacity: 1, duration:0.5, y: 0, delay: .1})
+      .to('.svg-txt path', {opacity: 1, duration:0.5, strokeDashoffset:0, strokeDasharray:85})
+      .to('.svg-txt path', {fill:"#fff"})
+      .to('.m_copy p', {opacity: 1, duration:0.5, y:0,})
+      .to('.scroll', {opacity: 1, duration:0.5});
+    }
+
   // 스크롤이벤트
   const about_tl = gsap.timeline({
     scrollTrigger: {
@@ -530,9 +555,7 @@
 
 
   $(window).scroll(function(){
-    const winSc=$(window).scrollTop();
-    const winMo = window.matchMedia("(max-width: 1024px)").matches;
-    
+    winSc = $(window).scrollTop();
     // 헤더 고정
     if(winSc > (winMo ? 0 : 780)) {
       $('header').addClass('head-fix');
